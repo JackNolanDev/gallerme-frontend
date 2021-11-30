9a7bacdb7b3af238bcafb2e376606b67a94b8d96
jacknolan <921Jack@gmail.com>
2021-11-30 12:33:31 -0500

    List art page DONE

---
+++ src/components/DisplayArt.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-bind:style="artBoardStyle" class="artBoard">
+    <div
+      v-for="(pixel, index) in pixels"
+      v-bind:key="index"
+      v-bind:style="{ backgroundColor: pixel }"
+      class="ratio ratio-1x1"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayArt",
+  props: {
+    art: Object,
+  },
+  computed: {
+    artBoardStyle() {
+      return { gridTemplateColumns: `repeat(${this.art.size}, 1fr)` };
+    },
+    pixels() {
+      const chunkCount = Math.ceil(this.art.data.length / 6);
+      const pixels = new Array(chunkCount);
+      for (let i = 0, o = 0; i < chunkCount; ++i, o += 6) {
+        pixels[i] = "#" + this.art.data.substr(o, 6);
+      }
+      return pixels;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.artBoard {
+  display: grid;
+}
+</style>
--- src/views/Admin/Art.vue
+++ src/views/Admin/Art.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-    <h1>Admin art page</h1>
+  <div class="container">
+    <h1 class="text-center mt-2">Art</h1>
+    <router-link to="/admin/art/new" class="btn btn-primary">
+      Create New Art
+    </router-link>
+    <div class="list-group mt-2">
+      <router-link
+        v-for="(art, index) in artList"
+        v-bind:key="index"
+        :to="{ name: 'AdminArtDetail', params: { id: art.id } }"
+        class="list-group-item list-group-item-action d-flex align-items-center"
+      >
+        <display-art v-bind:art="art" class="art me-2" />
+        <div>{{ art.name }}</div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import DisplayArt from "../../components/DisplayArt.vue";
+
 export default {
   name: "AdminArt",
+  components: {
+    DisplayArt,
+  },
   beforeMount() {
-    this.$store.dispatch("fetchAllArt");
+    this.$store.dispatch("art/fetchAllArt");
+  },
+  computed: {
+    ...mapState("art", ["artList"]),
   },
 };
 </script>
+
+<style scoped>
+.art {
+  width: 200px;
+}
+</style>
